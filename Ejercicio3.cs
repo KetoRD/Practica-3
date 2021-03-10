aff1b0f28cb5af686886cf28f3d0df465f4f52d4
KetoRD <78993548+KetoRD@users.noreply.github.com>
2021-03-10 01:20:32 -0400

    Add files via upload

---
+++ Ejercicio3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num, Cont;
+            Num = 10 * 10;
+            Cont = 0;
+            do
+            {
+                Num = Num + 2;
+                Cont++;
+                Console.WriteLine(Num);
+            } while (Cont < 10);
+        }
+    }
+}
